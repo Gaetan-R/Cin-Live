f98889807b566f35f418b1a467759453cb87e9eb
wildcodeschool <richer.gaetan33@gmail.com>
2021-01-13 13:33:25 +0100

    Add register

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -97,7 +97,7 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator implements P
         }
 
         // For example : return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        return new RedirectResponse($this->urlGenerator->generate(""));
+        return new RedirectResponse($this->urlGenerator->generate("program"));
     }
 
     protected function getLoginUrl()
